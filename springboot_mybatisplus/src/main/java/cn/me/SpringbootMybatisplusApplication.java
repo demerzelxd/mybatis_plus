b2f7037dccf1ab4ccda1d3d2eac33fa5aeee8972
demerzelxd <demerzelxd@gmail.com>
2020-11-02 10:21:06 +0800

    新建工程

---
+++ springboot_mybatisplus/src/main/java/cn/me/SpringbootMybatisplusApplication.java
@@ -0,0 +1,15 @@
+package cn.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootMybatisplusApplication.class, args);
+    }
+
+}
